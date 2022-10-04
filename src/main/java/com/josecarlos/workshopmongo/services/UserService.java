180348ed5cda51ff1c764947f8f010f13ec60962
José Carlos <josecarlospaivasantos2001@gmail.com>
2022-10-04 17:46:49 -0300

    add

---
+++ src/main/java/com/josecarlos/workshopmongo/services/UserService.java
@@ -0,0 +1,41 @@
+package com.josecarlos.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josecarlos.workshopmongo.domain.User;
+import com.josecarlos.workshopmongo.dto.UserDTO;
+import com.josecarlos.workshopmongo.repository.UserRepository;
+import com.josecarlos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void deleteById(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
